00b8fc26908ddc7f6e9df55a374d5d489b398dfe
ptabis <p.tabis@protonmail.com>
2020-05-02 15:47:55 +0200

    add Gallery and Page to router

--- src/js/router.js
+++ src/js/router.js
@@ -1,6 +1,10 @@
 import m from 'mithril';
 import Home from './components/Home';
+import Gallery from './components/Gallery';
+import Page from './components/Page';
 
 m.route(document.querySelector('#app'), '/', {
-    '/': Home
+    '/': Home,
+    '/g/:gallery_id': Gallery,
+    '/g/:gallery_id/page/:page_id': Page,
 });
